207bcca1ecebd48fd52c2ee86c2970220f6b62b8
vvbakhanovich <vv.bakh@yandex.ru>
2023-08-12 15:58:12 +0300

    feat: Добабвить метод checkEpicStatus

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -1,10 +1,46 @@
 package tasks;
 
+import java.util.ArrayList;
+
 public class Subtask extends Task{
-    private Epic epic;
+    private final Epic epic;
 
     public Subtask(String taskName, String description, Status status, Epic epic) {
         super(taskName, description, status);
         this.epic = epic;
     }
+
+    public Epic getEpic() {
+        return epic;
+    }
+
+    public void checkEpicStatus() {
+
+        ArrayList<Subtask> subtasks = epic.getSubtaskList();
+        if (subtasks.isEmpty()) {
+            epic.setStatus(Status.NEW);
+        }
+
+        int statusDone = 0;
+        int statusNew = 0;
+
+        for (Subtask subtask : subtasks) {
+            switch (subtask.getStatus()) {
+                case NEW:
+                    statusNew++;
+                    break;
+                case DONE:
+                    statusDone++;
+                    break;
+            }
+        }
+        if (statusDone == subtasks.size()) {
+            epic.setStatus(Status.DONE);
+        } else if (statusNew == subtasks.size()) {
+            epic.setStatus(Status.NEW);
+        } else {
+            epic.setStatus(Status.IN_PROGRESS);
+        }
+    }
+
 }

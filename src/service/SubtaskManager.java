60a26e935894a144d29e9b7bc57570de44b1e271
vvbakhanovich <vv.bakh@yandex.ru>
2023-08-14 16:27:02 +0300

    refactor: Добавить класс SubtaskManager

---
+++ src/service/SubtaskManager.java
@@ -0,0 +1,42 @@
+package service;
+
+import model.Epic;
+import model.Subtask;
+
+public class SubtaskManager extends BasicManager<Subtask> {
+    @Override
+    public void addTask(Subtask subtask) {
+        int id = generateId();
+        subtask.setTaskId(id);
+        taskList.put(id, subtask);
+
+        Epic epic = subtask.getEpic();
+        epic.getSubtaskList().add(subtask);
+        subtask.checkEpicStatus();
+    }
+
+    @Override
+    public void updateTask(int taskId, Subtask subtask) {
+        Subtask currentSubtask = taskList.getOrDefault(taskId, null);
+        if (isNullTask(currentSubtask)) {
+            return;
+        }
+        Epic epic = subtask.getEpic();
+        int index = epic.getSubtaskList().indexOf(currentSubtask);
+        taskList.put(taskId, subtask);
+        epic.getSubtaskList().set(index, subtask);
+        subtask.checkEpicStatus();
+    }
+
+    @Override
+    public void removeTaskById(int taskId) {
+        Subtask subtask = taskList.getOrDefault(taskId, null);
+        if (isNullTask(subtask)) {
+            return;
+        }
+        Epic epic = subtask.getEpic();
+        taskList.remove(taskId);
+        epic.getSubtaskList().remove(subtask);
+        subtask.checkEpicStatus();
+    }
+}

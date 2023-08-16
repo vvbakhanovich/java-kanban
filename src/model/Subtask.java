b2ec0e6cba994a24539aab5c6ecb98e612411aa4
vvbakhanovich <vv.bakh@yandex.ru>
2023-08-16 19:55:56 +0300

    delete: Удалить метод checkEpicStatus()

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -16,37 +16,6 @@ public class Subtask extends Task{
         return epic;
     }
 
-    // метод для обновления статуса эпика при операциях с подзадачами
-    public void checkEpicStatus() {
-
-        ArrayList<Subtask> subtasks = epic.getSubtaskList();
-        if (subtasks.isEmpty()) {
-            epic.setStatus(Status.NEW);
-            return;
-        }
-
-        int statusDone = 0;
-        int statusNew = 0;
-
-        for (Subtask subtask : subtasks) {
-            switch (subtask.getStatus()) {
-                case NEW:
-                    statusNew++;
-                    break;
-                case DONE:
-                    statusDone++;
-                    break;
-            }
-        }
-        if (statusDone == subtasks.size()) {
-            epic.setStatus(Status.DONE);
-        } else if (statusNew == subtasks.size()) {
-            epic.setStatus(Status.NEW);
-        } else {
-            epic.setStatus(Status.IN_PROGRESS);
-        }
-    }
-
     @Override
     public String toString() {
         return "Subtask{" +

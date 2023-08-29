9f10fc3c53cf55fe6200cf26832fa140c33a75dd
vvbakhanovich <vv.bakh@yandex.ru>
2023-08-29 19:09:56 +0300

    feature: Переопределить equals/hashcode в Epic

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -2,6 +2,7 @@ package tasks;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Epic extends Task {
 
@@ -20,6 +21,20 @@ public class Epic extends Task {
     // возвращает копию списка подзадач
     public List<Long> getSubtaskList() {
         return subtaskList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Epic epic = (Epic) o;
+        return Objects.equals(subtaskList, epic.subtaskList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), subtaskList);
     }
 
     @Override

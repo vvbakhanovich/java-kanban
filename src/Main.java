932b8b4137f2124da63d9814c720d48df5ed40e6
vvbakhanovich <vv.bakh@yandex.ru>
2023-09-16 14:58:43 +0300

    feature: Добавить тесты в main

--- src/Main.java
+++ src/Main.java
@@ -10,70 +10,101 @@ public class Main {
     public static void main(String[] args) {
         TaskManager manager = Managers.getDefault();
 
-        // создаем две простые задачи
+        // создаем и добавляем две простые задачи
         BasicTask task1 = BasicTask.create("Задача 1", "Описание задачи 1", Status.NEW);
-        manager.addBasicTask(task1);
+        long task1Id = manager.addBasicTask(task1);
         BasicTask task2 = BasicTask.create("Задача 2", "Описание задачи 2", Status.NEW);
-        manager.addBasicTask(task2);
+        long task2Id = manager.addBasicTask(task2);
 
-        // вызываем метод getBasicTaskById, чтобы задача внеслись в историю просмотров
-        manager.getBasicTaskById(task1.getTaskId());
-        manager.getBasicTaskById(task1.getTaskId());
-
-        //печатаем историю просмотров
-        System.out.println("\nИстория просмотров после прочтения двух простых задач");
-        System.out.println(manager.getHistory());
-        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
-
-        //создаем эпик и две его подзадачи
+        //создаем и добавляем эпик с тремя подзадачами
         Epic epic1 = Epic.create("Эпик 1", "Описание эпика 1");
-        manager.addEpic(epic1);
+        long epic1Id = manager.addEpic(epic1);
 
         Subtask subtask1 = Subtask.create("Подазадача 1", "Описание подзадачи 1",
-                Status.NEW, epic1.getTaskId());
+                Status.NEW, epic1Id);
         Subtask subtask2 = Subtask.create("Подзадача 2", "Описание подзадачи 2",
-                Status.NEW, epic1.getTaskId());
-        manager.addSubtask(subtask1);
-        manager.addSubtask(subtask2);
-
-        //получаем созданные задачи
-        manager.getEpicById(epic1.getTaskId());
-        manager.getSubtaskById(subtask1.getTaskId());
-        manager.getSubtaskById(subtask2.getTaskId());
-
-        System.out.println("\n===================\n");
-        System.out.println("После просмотра эпика и двух подзадач, история просмотров:");
-        System.out.println(manager.getHistory());
-        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
-
-        //создаем еще один эпик с одной подзадачей
-        Epic epic2 = Epic.create("Эпик 2", "Описание эпика 2");
-        manager.addEpic(epic2);
-
+                Status.NEW, epic1Id);
         Subtask subtask3 = Subtask.create("Подзадача 3", "Описание подзадачи 3",
-                Status.NEW, epic2.getTaskId());
-        manager.addSubtask(subtask3);
+                Status.NEW, epic1Id);
+        long subtask1Id = manager.addSubtask(subtask1);
+        long subtask2Id = manager.addSubtask(subtask2);
+        long subtask3Id = manager.addSubtask(subtask3);
 
-        //считываем задачи
-        manager.getEpicById(epic2.getTaskId());
-        manager.getSubtaskById(subtask3.getTaskId());
+        //создаем и добавляем эпик без подзадач
+        Epic epic2 = Epic.create("Эпик 2", "Описание эпика 2");
+        long epic2Id = manager.addEpic(epic2);
 
+        // вызываем метод getBasicTaskById, чтобы задача task1 внеслась в историю просмотров
+        manager.getBasicTaskById(task1Id);
+        //печатаем список просмотров
         System.out.println("\n===================\n");
-        System.out.println("После просмотра эпика и одной подзадачи, история просмотров:");
+        System.out.println("После просмотра task1, история просмотров:");
         System.out.println(manager.getHistory());
         System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
 
+        //просматриваем 2й эпик
+        manager.getEpicById(epic2Id);
 
-        //прочитаем еще пять любых задач, чтобы проверить, что размер списка равен 10 и задачи, добавленные первыми,
-        //были вытеснены последующими задачами
-        manager.getBasicTaskById(task1.getTaskId());
-        manager.getBasicTaskById(task2.getTaskId());
-        manager.getSubtaskById(subtask1.getTaskId());
-        manager.getSubtaskById(subtask2.getTaskId());
-        manager.getSubtaskById(subtask3.getTaskId());
-        System.out.println("\n=============\n");
-        System.out.println("После считывания еще 5 задач, история просмотров:");
+        //печатаем историю просмотров
+        System.out.println("\n===================\n");
+        System.out.println("После просмотра epic2, история просмотров:");
         System.out.println(manager.getHistory());
-        System.out.println(manager.getHistory().size());
+        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
+
+        //просматриваем оставшиеся задачи
+        manager.getEpicById(epic1Id);
+        manager.getSubtaskById(subtask1Id);
+        manager.getSubtaskById(subtask2Id);
+        manager.getSubtaskById(subtask3Id);
+        manager.getBasicTaskById(task2Id);
+        manager.getEpicById(epic2Id);
+
+        System.out.println("\n===================\n");
+        System.out.println("После однократного просмотра оставшихся задач:");
+        System.out.println(manager.getHistory());
+        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
+
+        manager.getBasicTaskById(task1Id);
+        System.out.println("\n===================\n");
+        System.out.println("После повторного просмотра task1 (из начала списка):");
+        System.out.println(manager.getHistory());
+        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
+
+        manager.getSubtaskById(subtask3Id);
+        System.out.println("\n===================\n");
+        System.out.println("После повторного просмотра subtask3 (из середины списка):");
+        System.out.println(manager.getHistory());
+        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
+
+        manager.getSubtaskById(subtask3Id);
+        System.out.println("\n===================\n");
+        System.out.println("После еще одного просмотра subtask3 (из конца списка):");
+        System.out.println(manager.getHistory());
+        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
+
+        //тесты по удалению задач
+        manager.removeEpicById(epic1Id);
+        System.out.println("\n===================\n");
+        System.out.println("После удаления epic1 (из начала списка):");
+        System.out.println(manager.getHistory());
+        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
+
+        manager.removeSubtaskById(subtask3Id);
+        System.out.println("\n===================\n");
+        System.out.println("После удаления subtask3 (из конца списка):");
+        System.out.println(manager.getHistory());
+        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
+
+        manager.removeBasicTaskById(task2Id);
+        System.out.println("\n===================\n");
+        System.out.println("После удаления task2 (из середины списка):");
+        System.out.println(manager.getHistory());
+        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
+
+        manager.removeAllEpics();
+        System.out.println("\n===================\n");
+        System.out.println("После удаления всех эпиков:");
+        System.out.println(manager.getHistory());
+        System.out.println("Количество просмотренных задач:" + manager.getHistory().size());
     }
 }

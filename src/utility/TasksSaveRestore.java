66e8d39eb0f89552645b0f4addc6a42de36b6c57
vvbakhanovich <vv.bakh@yandex.ru>
2023-10-04 19:25:13 +0300

    style: Добавить javadoc

--- src/utility/TasksSaveRestore.java
+++ src/utility/TasksSaveRestore.java
@@ -1,18 +1,24 @@
-package service;
+package utility;
 
 import manager.HistoryManager;
 import tasks.*;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TasksSaveRestore {
+/**
+ * Вспомогательный класс для преобразования задач в строки и обратно.
+ */
+public final class TasksSaveRestore {
 
+    private TasksSaveRestore() {}
+
+    /**
+     * Преобразование задачи в строку формата csv
+     * @param task задача, которую требуется преобразовать
+     * @return строковое представление задачи
+     */
     public static String taskToString(Task task) {
-
         if (task instanceof BasicTask) {
             return String.format("%d,%s,%s,%s,%s\n", task.getTaskId(), task.getTaskType(), task.getTaskName(),
                     task.getDescription(), task.getStatus());
@@ -23,11 +29,17 @@ public class TasksSaveRestore {
             Subtask subtask = (Subtask) task;
             return String.format("%d,%s,%s,%s,%s,%s\n", subtask.getTaskId(), subtask.getTaskType(), subtask.getTaskName(),
                     subtask.getDescription(), subtask.getStatus(), subtask.getEpicId());
+        } else {
+            return null;
         }
-        return null;
     }
 
-    public static Task stringToTask(String value) throws NoSuchFieldException{
+    /**
+     * Преобразование строки csv файла в задачу.
+     * @param value строка, из которой требуется получить задачу
+     * @return задача, созданная из csv строки
+     */
+    public static Task stringToTask(String value){
         String[] task = value.split(",");
         TaskTypes type = TaskTypes.valueOf(task[1]);
 
@@ -40,10 +52,15 @@ public class TasksSaveRestore {
                 return Subtask.createFromFile(Long.parseLong(task[0]), task[2], task[3], Status.valueOf(task[4]),
                         Long.parseLong(task[5]));
             default:
-                throw new NoSuchFieldException("Некорректный тип задачи: " + type);
+                return null;
         }
     }
 
+    /**
+     * Преобразование истории в строку формата csv. Строка состоит из id просмотренных задач
+     * @param historyManager менеджер истории, чьи просмотры необходимо сохранить
+     * @return строка, в которой содержатся id просмотренных задач
+     */
     public static String historyToString(HistoryManager historyManager) {
         StringBuilder sb = new StringBuilder();
         String separator = "";
@@ -54,6 +71,11 @@ public class TasksSaveRestore {
         return sb.toString();
     }
 
+    /**
+     * Преобразование строки формата csv в список id задач.
+     * @param value строка, состоящая из id просмотренных задач
+     * @return список id задач из истории просмотров
+     */
     public static List<Long> historyFromString(String value) {
         List<Long> result = new ArrayList<>();
         String[] historyId = value.split(",");

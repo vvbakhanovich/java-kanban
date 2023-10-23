4a79016784acc50ac425825d04f1223a21ec62e4
vvbakhanovich <vv.bakh@yandex.ru>
2023-10-23 19:59:45 +0300

    feat: Создать утилитарный класс, перенести FORMATTER

---
+++ src/utility/Constants.java
@@ -0,0 +1,12 @@
+package utility;
+
+import java.time.format.DateTimeFormatter;
+
+public final class Constants {
+
+    private Constants() {}
+
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+}
+
+

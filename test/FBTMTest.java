6003e2e0fc39340879db3b9ccbaa55cfb91f4932
vvbakhanovich <vv.bakh@yandex.ru>
2023-10-24 05:24:18 +0300

    refactor: Удалить неизмененные методы

--- test/FBTMTest.java
+++ test/FBTMTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class FBTMTest extends AbstractTaskManagerTest {
+
     @Override
     @BeforeEach
     void setManager() {

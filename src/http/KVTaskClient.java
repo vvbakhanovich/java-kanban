21689830390d76d56d775e7343858adee0d817e7
vvbakhanovich <vv.bakh@yandex.ru>
2023-10-30 19:38:25 +0300

    feat: Добавить KVSTaskClient

---
+++ src/http/KVTaskClient.java
@@ -0,0 +1,59 @@
+package http;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class KVTaskClient {
+    private String API_TOKEN;
+    private URI serverUri;
+    private final HttpClient httpClient;
+
+    public KVTaskClient(String uri) throws IOException, InterruptedException {
+        serverUri = URI.create(uri);
+        URI registerUri = URI.create(uri + "/register");
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(registerUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        httpClient = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        API_TOKEN = response.body();
+    }
+
+    //POST /save/<key>?API_TOKEN_
+    public void put(String key, String json) throws IOException, InterruptedException {
+        URI saveUri = URI.create(serverUri + "/save/" + key + "?API_TOKEN=" + API_TOKEN);
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .uri(saveUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpResponse<String> response = httpClient.send(request, handler);
+
+    }
+    //GET /load/<key>?API_TOKEN_
+    public String load(String key) throws IOException, InterruptedException {
+        URI loadUri = URI.create(serverUri + "/load/" + key + "?API_TOKEN=" + API_TOKEN);
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(loadUri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpResponse<String> response = httpClient.send(request, handler);
+        if (response.statusCode() == 200) {
+            return response.body();
+        } else {
+
+        }
+
+        return "";
+    }
+
+}

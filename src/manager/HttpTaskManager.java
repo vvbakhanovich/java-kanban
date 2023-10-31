0a370a45ff4585bf70e16c6540fbbe72e0718baa
vvbakhanovich <vv.bakh@yandex.ru>
2023-10-31 20:36:24 +0300

    feat: Добавить load

--- src/manager/HttpTaskManager.java
+++ src/manager/HttpTaskManager.java
@@ -2,17 +2,21 @@ package manager;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import http.KVTaskClient;
 import http.LocalDateTimeAdapter;
+import tasks.BasicTask;
 
+import java.lang.reflect.Type;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class HttpTaskManager extends FileBackedTasksManager {
 
     private final KVTaskClient taskClient;
     private final Gson gson;
     public HttpTaskManager(String uri) {
-        super("resources/FBTM_test.csv");
+        super(null);
         taskClient = new KVTaskClient(uri);
         gson = new GsonBuilder()
                 .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
@@ -20,12 +24,17 @@ public class HttpTaskManager extends FileBackedTasksManager {
     }
 
     @Override
-    protected void save() {
+    public void save() {
         taskClient.put("BasicTasks", gson.toJson(getBasicTaskList()));
         taskClient.put("Epics", gson.toJson(getEpicList()));
         taskClient.put("Subtasks", gson.toJson(getSubtaskList()));
         taskClient.put("History", gson.toJson(getHistory()));
         taskClient.put("PrioritizedList", gson.toJson(getPrioritizedTasks()));
+    }
+
+    public void load() {
+        Type taskListType = new TypeToken<ArrayList<BasicTask>>(){}.getType();
+        ArrayList<BasicTask> basicTaskList = gson.fromJson(taskClient.load("BasicTasks"), taskListType);
     }
 
 }

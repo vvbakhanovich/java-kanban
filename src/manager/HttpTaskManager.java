d05d08775c6849365a930b3b6febc72a11f49ba0
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-09 19:43:50 +0300

    feat: Сериализовывать Task

--- src/manager/HttpTaskManager.java
+++ src/manager/HttpTaskManager.java
@@ -11,7 +11,6 @@ import tasks.Task;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class HttpTaskManager extends FileBackedTasksManager {
     //список ключей для хранения и запроса данных с KVServer
@@ -48,12 +47,8 @@ public class HttpTaskManager extends FileBackedTasksManager {
         taskClient.put(basicTaskKey, gson.toJson(getBasicTaskList()));
         taskClient.put(epicKey, gson.toJson(getEpicList()));
         taskClient.put(subtaskKey, gson.toJson(getSubtaskList()));
-        taskClient.put(historyKey, gson.toJson(getHistory().stream()
-                .map(Task::getTaskId)
-                .collect(Collectors.toList())));
-        taskClient.put(sortedKey, gson.toJson(getPrioritizedTasks().stream()
-                .map(Task::getTaskId)
-                .collect(Collectors.toList())));
+        taskClient.put(historyKey, gson.toJson(getHistory()));
+        taskClient.put(sortedKey, gson.toJson(getPrioritizedTasks()));
         taskClient.put(idKey, gson.toJson(taskId));
     }
 
@@ -65,64 +60,44 @@ public class HttpTaskManager extends FileBackedTasksManager {
 
         manager.taskId = gson.fromJson(taskClient.load(idKey), Long.class);
 
-        Type taskListType = new TypeToken<ArrayList<BasicTask>>() {
-        }.getType();
+        Type taskListType = new TypeToken<ArrayList<BasicTask>>() {}.getType();
         ArrayList<BasicTask> basicTaskList = gson.fromJson(taskClient.load(basicTaskKey), taskListType);
         for (BasicTask basicTask : basicTaskList) {
             manager.basicTaskList.put(basicTask.getTaskId(), basicTask);
         }
 
-        Type epicListType = new TypeToken<ArrayList<Epic>>() {
-        }.getType();
+        Type epicListType = new TypeToken<ArrayList<Epic>>() {}.getType();
         ArrayList<Epic> epicList = gson.fromJson(taskClient.load(epicKey), epicListType);
         for (Epic epic : epicList) {
             manager.epicList.put(epic.getTaskId(), epic);
         }
 
-        Type subtaskListType = new TypeToken<ArrayList<Subtask>>() {
-        }.getType();
+        Type subtaskListType = new TypeToken<ArrayList<Subtask>>() {}.getType();
         ArrayList<Subtask> subtaskList = gson.fromJson(taskClient.load(subtaskKey), subtaskListType);
         for (Subtask subtask : subtaskList) {
             manager.subtaskList.put(subtask.getTaskId(), subtask);
         }
 
-        Type sortedSetType = new TypeToken<ArrayList<Long>>() {
-        }.getType();
-        ArrayList<Long> sortedTaskSet = gson.fromJson(taskClient.load(sortedKey), sortedSetType);
-        restorePrioritizedList(sortedTaskSet, manager);
-
-
-        Type historyIdType = new TypeToken<ArrayList<Long>>() {
-        }.getType();
-        ArrayList<Long> historyIdList = gson.fromJson(taskClient.load(historyKey), historyIdType);
+        Type historyIdType = new TypeToken<ArrayList<Task>>() {}.getType();
+        ArrayList<Task> historyIdList = gson.fromJson(taskClient.load(historyKey), historyIdType);
         restoreHistory(historyIdList, manager);
+
+        Type sortedSetType = new TypeToken<ArrayList<Task>>() {}.getType();
+        ArrayList<Task> sortedTaskSet = gson.fromJson(taskClient.load(sortedKey), sortedSetType);
+        restorePrioritizedList(sortedTaskSet, manager);
     }
 
 
-    private static void restoreHistory(List<Long> historyIds, HttpTaskManager manager) {
-        for (Long taskId : historyIds) {
 
-            if (manager.basicTaskList.containsKey(taskId)) {
-                manager.historyManager.add(manager.basicTaskList.get(taskId));
-            } else if (manager.epicList.containsKey(taskId)) {
-                manager.historyManager.add(manager.epicList.get(taskId));
-            } else {
-                manager.historyManager.add(manager.subtaskList.get(taskId));
-            }
+
+    private static void restoreHistory(List<Task> historyIds, HttpTaskManager manager) {
+        for (Task task : historyIds) {
+            manager.historyManager.add(task);
         }
     }
 
-    private static void restorePrioritizedList(List<Long> historyIds, HttpTaskManager manager) {
-        for (Long taskId : historyIds) {
-
-            if (manager.basicTaskList.containsKey(taskId)) {
-                manager.sortedTasks.add(manager.basicTaskList.get(taskId));
-            } else if (manager.epicList.containsKey(taskId)) {
-                manager.sortedTasks.add(manager.epicList.get(taskId));
-            } else {
-                manager.sortedTasks.add(manager.subtaskList.get(taskId));
-            }
-        }
+    private static void restorePrioritizedList(List<Task> sortedTasks, HttpTaskManager manager) {
+        manager.sortedTasks.addAll(sortedTasks);
     }
 
 }
